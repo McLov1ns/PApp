1c98fb223ab9549a758def84fc8a83d27b31030f
Alex Dem <deminas@mer.ci.nsu.ru>
2024-12-02 20:00:04 +0700

    Добавьте файлы проекта.

---
+++ PApp/TestData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PApp
+{
+    public static class TestData
+    {
+        public static string GetTextFromFile(string path)
+        {
+            return File.ReadAllText(path);
+        }
+    }
+}
---
+++ PAppTests/StringTests.cs
@@ -0,0 +1,46 @@
+﻿using PApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PautTests
+{
+    public class StringTests
+    {
+        [Theory]
+        [InlineData("Hello world", 2)]
+        [InlineData("This is a test", 4)]
+        [InlineData("Count the words", 3)]
+        public void TestWordCount(string text, int expected)
+        {
+            Assert.Equal(expected, Calculations.WordCount(text));
+        }
+
+        [Theory]
+        [InlineData("Hello world", "world", true)]
+        [InlineData("This is a test", "python", false)]
+        [InlineData("Find the substring", "substring", true)]
+        public void TestSubstring(string text, string sub, bool expected)
+        {
+            Assert.Equal(expected, Calculations.Substring(text, sub));
+        }
+
+        [Theory]
+        [InlineData("Hello world", "HELLO WORLD")]
+        [InlineData("Python", "PYTHON")]
+        [InlineData("To UpperCase", "TO UPPERCASE")]
+        public void TestToUpperCase(string text, string expected)
+        {
+            Assert.Equal(expected, Calculations.ToUpperCase(text));
+        }
+
+        [Fact]
+        public void TestFileReading()
+        {
+            string text = TestData.GetTextFromFile("C:\\Users\\MEGABOOK\\source\\repos\\PApp\\PApp\\sample.txt");
+            Assert.NotNull(text);
+        }
+    }
+}
